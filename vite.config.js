21e430eb9063245fb4a830454789548f9f1268a7
KashishParveen <kashishparveen28@gmail.com>
2025-09-27 15:47:39 +0530

    config: Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite';
-
 export default defineConfig({
+  // 🚨 ADD THIS LINE 🚨
+  base: '/KeshavSoft-task2/', 
+  // --------------------
   root: 'src',
   build: {
     outDir: '../dist',
     emptyOutDir: false
-  }
+}
 });
